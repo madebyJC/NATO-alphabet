74ff87ebdcea537bf94c78baa9559eb940fae519
Jc Paredes <paredesjancarlo99@gmail.com>
2022-08-14 21:13:21 +0800

    added list result

--- main.py
+++ main.py
@@ -11,4 +11,5 @@ new_data = {row.letter: row.code for (index, row) in data.iterrows()}
 input_word = input("Enter a word: ").upper()
 result = [new_data[letter] for letter in input_word]
 final_result = " ".join(result)
+print(result)
 print(final_result)
