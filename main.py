6b1d9c68fb31af92606a27f6cc85d43b81c53018
Jc Paredes <paredesjancarlo99@gmail.com>
2022-08-14 21:23:22 +0800

    added error handling and while loops

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ student_dict = {
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 new_data = {row.letter: row.code for (index, row) in data.iterrows()}
 
-input_word = input("Enter a word: ").upper()
-result = [new_data[letter] for letter in input_word]
-final_result = " ".join(result)
-print(result)
-print(final_result)
+user_continue = True
+while user_continue:
+    try:
+        input_word = input("Enter a word: ").upper()
+        result = [new_data[letter] for letter in input_word]
+        final_result = " ".join(result)
+    except KeyError as key_error:
+        print(f"The input {key_error} is not supported, please enter a letter in the alphabet.")
+    else:
+        print(result)
+        print(final_result)
+        user_continue = False
